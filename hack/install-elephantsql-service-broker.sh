95f1559ff8297aa4652b5b44a98371d6175fc14a
Toshiaki Maki <tmaki@pivotal.io>
2020-06-03 21:27:34 +0900

    Fix install-elephantsql-service-broker.sh

--- hack/install-elephantsql-service-broker.sh
+++ hack/install-elephantsql-service-broker.sh
@@ -5,15 +5,14 @@ cf create-org elephantsql
 cf create-space elephantsql -o elephantsql
 cf target -o elephantsql -s elephantsql
 
-SPRING_SECURITY_USER_PASSWORD=$(mktemp | sed 's|/||g')
 wget -O elephantsql-service-broker.yml https://github.com/making/elephantsql-service-broker/raw/master/manifest.yml
 cf push --no-start -f elephantsql-service-broker.yml
 cf set-env elephantsql-service-broker ELEPHANTSQL_API_KEY ${ELEPHANTSQL_API_KEY}
-cf set-env elephantsql-service-broker SPRING_SECURITY_USER_PASSWORD ${SPRING_SECURITY_USER_PASSWORD}
+cf set-env elephantsql-service-broker SPRING_SECURITY_USER_PASSWORD ${ELEPHANTSQL_API_KEY}
 cf start elephantsql-service-broker
 
 set +e
-cf create-service-broker elephantsql admin ${SPRING_SECURITY_USER_PASSWORD} https://elephantsql-service-broker.$(bosh int $(dirname $0)/../configuration-values/deployment-values.yml --path /app_domains/0)
+cf create-service-broker elephantsql admin ${ELEPHANTSQL_API_KEY} https://elephantsql-service-broker.$(bosh int $(dirname $0)/../configuration-values/deployment-values.yml --path /app_domains/0)
 set -e
 cf enable-service-access elephantsql
 rm -f elephantsql-service-broker.yml
