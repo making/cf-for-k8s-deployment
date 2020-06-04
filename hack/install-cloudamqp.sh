e4a5c689c4ba1de37e0ae844f52952605f06dbee
Toshiaki Maki <tmaki@pivotal.io>
2020-06-05 02:50:11 +0900

    Use cf-for-k8s 0.3.0

---
+++ hack/install-cloudamqp.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -euox pipefail
+
+cf create-org cloudamqp
+cf create-space cloudamqp -o cloudamqp
+cf target -o cloudamqp -s cloudamqp
+
+git clone https://github.com/making/cloudamqp-service-broker
+cd cloudamqp-service-broker
+cat <<EOF > cloudamqp-service-broker.yml
+applications:
+- name: cloudamqp-service-broker
+  instances: 1
+  memory: 256M
+  env:
+    JAVA_OPTS: -XX:ReservedCodeCacheSize=32M -Xss512k -Duser.timezone=Asia/Tokyo
+    SERVICE_BROKER_ADMIN_PASSWORD: ${CLOUDAMQP_BROKER_ADMIN_PASSWORD}
+    SERVICE_BROKER_CLOUDAMQP_APIKEY: ${SERVICE_BROKER_CLOUDAMQP_APIKEY}
+    BPL_THREAD_COUNT: 20
+    BPL_JVM_THREAD_COUNT: 20
+EOF
+cf push cloudamqp-service-broker -f cloudamqp-service-broker.yml
+set +e
+cf create-service-broker cloudamqp admin ${CLOUDAMQP_BROKER_ADMIN_PASSWORD} https://cloudamqp-service-broker.${APPS_DOMAIN}
+set -e
+cf enable-service-access cloudamqp
+
+rm -f cloudamqp-service-broker.jar
+rm -f cloudamqp-service-broker.yml
