0a29817944978c8f391b5d942f3d85ee31ac0b42
Toshiaki Maki <tmaki@pivotal.io>
2020-06-22 20:02:15 +0900

    _deps -> _ytt_lib

--- hack/smoke-test.sh
+++ hack/smoke-test.sh
@@ -6,4 +6,4 @@ export SMOKE_TEST_USERNAME=admin
 export SMOKE_TEST_PASSWORD=$(bosh int $(dirname $0)/../configuration-values/deployment-values.yml  --path /cf_admin_password)
 export SMOKE_TEST_SKIP_SSL=true
 
-$(dirname $0)/../config/_deps/cf-for-k8s/hack/run-smoke-tests.sh
+$(dirname $0)/../config/_ytt_lib/cf-for-k8s/hack/run-smoke-tests.sh
