650580684fde12338549262b4620588143ada332
Toshiaki Maki <tmaki@pivotal.io>
2020-08-04 10:38:42 +0900

    Bump to cf-for-k8s v0.5.0

--- config/_deps/cf-for-k8s/hack/generate-values.sh
+++ config/_deps/cf-for-k8s/hack/generate-values.sh
@@ -95,11 +95,17 @@ variables:
   type: password
 - name: capi_db_password
   type: password
+- name: capi_db_encryption_key
+  type: password
 - name: uaa_db_password
   type: password
 - name: uaa_admin_client_secret
   type: password
 - name: uaa_encryption_key_passphrase
+  type: password
+- name: cc_username_lookup_client_secret
+  type: password
+- name: cf_api_controllers_client_secret
   type: password
 - name: default_ca
   type: certificate
@@ -216,8 +222,11 @@ cf_db:
   admin_password: $(bosh interpolate ${VARS_FILE} --path=/db_admin_password)
 
 capi:
+  cc_username_lookup_client_secret: $(bosh interpolate ${VARS_FILE} --path=/cc_username_lookup_client_secret)
+  cf_api_controllers_client_secret: $(bosh interpolate ${VARS_FILE} --path=/cf_api_controllers_client_secret)
   database:
     password: $(bosh interpolate ${VARS_FILE} --path=/capi_db_password)
+    encryption_key: $(bosh interpolate ${VARS_FILE} --path=/capi_db_encryption_key)
 
 system_certificate:
   #! This certificates and keys are base64 encoded and should be valid for *.system.cf.example.com
