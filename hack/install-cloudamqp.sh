f15b257e9c1824a335ad9ecda2c0a1646c1b7751
Toshiaki Maki <tmaki@pivotal.io>
2020-09-07 22:45:46 +0900

    Update

--- hack/install-cloudamqp.sh
+++ hack/install-cloudamqp.sh
@@ -12,9 +12,11 @@ cat <<EOF > cloudamqp-service-broker.yml
 applications:
 - name: cloudamqp-service-broker
   instances: 1
+  docker:
+    image: making/cloudamqp-service-broker
   memory: 256M
   env:
-    JAVA_OPTS: -XX:ReservedCodeCacheSize=32M -Xss512k -Duser.timezone=Asia/Tokyo -Dservice-broker.admin.password=${SERVICE_BROKER_CLOUDAMQP_APIKEY} -Dservice-broker.cloudamqp.api-key=${SERVICE_BROKER_CLOUDAMQP_APIKEY}
+    JAVA_TOOL_OPTIONS: -XX:ReservedCodeCacheSize=32M -Xss512k -Duser.timezone=Asia/Tokyo -Dservice-broker.admin.password=${SERVICE_BROKER_CLOUDAMQP_APIKEY} -Dservice-broker.cloudamqp.api-key=${SERVICE_BROKER_CLOUDAMQP_APIKEY}
     BPL_THREAD_COUNT: 20
     BPL_JVM_THREAD_COUNT: 20
 EOF
